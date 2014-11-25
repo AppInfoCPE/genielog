c52195bf6637235d6c10a93a8b1cfa8898045a2c
julienbriot <briotjul@gmail.com>
2014-11-25 16:35:27 +0100

    MAJ interface de connexion avec image etc

--- Developpement/workspace/24_24_Manager/src/Manager24_24.java
+++ Developpement/workspace/24_24_Manager/src/Manager24_24.java
@@ -3,9 +3,9 @@
 public class Manager24_24 {
 	
 	public static void main(String[] args) {
-		LogicielConnection mc = new LogicielConnection();
+		LogicielConnection lc = new LogicielConnection();
 		// ICI c'est simplement le main, il faut réfléchier à mettre un niveau supplémentaire, 
 		// donc un Controlleur pere des autres en gros pour faciliter le passage entre les différentes fenetres
-		InterfaceConnection ic = new InterfaceConnection(mc);
+		InterfaceConnection ic = new InterfaceConnection(lc);
 	}
 }
