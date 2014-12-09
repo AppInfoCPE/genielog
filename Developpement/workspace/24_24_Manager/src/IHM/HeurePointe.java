ba9a9e8c3b8348b158dec2d23a286583aee485c3
longledac <long.ledac@gmail.com>
2014-12-09 14:46:16 +0100

    table_config manager

---
+++ Developpement/workspace/24_24_Manager/src/IHM/HeurePointe.java
@@ -0,0 +1,58 @@
+package IHM;
+
+
+
+public class HeurePointe {
+private  int id;
+static int id_bis=0; 
+private String debut;
+private String fin;
+
+	HeurePointe(String debut, String fin){
+		
+		 
+	 	/*int i = id_bis;
+	 	 id_bis ++; 
+	        */
+		
+		this.setDebut(debut);
+		this.setFin(fin);
+		
+	}
+        
+        HeurePointe(HeurePointe hp){
+            this.debut=hp.getDebut();
+            this.fin=hp.getFin();
+        }
+        HeurePointe() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getDebut() {
+		return debut;
+	}
+	
+	public void setDebut(String debut) {
+		this.debut = debut;
+	}
+	
+	public String getFin() {
+		return fin;
+	}
+	
+	public void setFin(String fin) {
+		this.fin = fin;
+	}
+        
+        public String toString(){
+         return this.getDebut()+ " - " + this.getFin();
+        }
+}
---
+++ Developpement/workspace/24_24_Manager/src/IHM/modelTable.java
@@ -0,0 +1,108 @@
+package IHM;
+
+import javax.swing.JButton;
+import javax.swing.table.AbstractTableModel;
+
+//je fais un model Table pour bien gerer Jtable car nos jtable sont dynamique (ajouter/supprimer les lignes)
+public class modelTable extends AbstractTableModel{
+    
+    private Object[][] data;
+    private String[] title;
+
+    //Constructeur
+    public modelTable(Object[][] data, String[] title){
+      this.data = data;
+      this.title = title;
+    }
+
+    //Retourne le nombre de colonnes
+    public int getColumnCount() {
+      return this.title.length;
+    }
+
+    //Retourne le nombre de lignes
+    public int getRowCount() {
+      return this.data.length;
+    }
+
+    //Retourne la valeur à l'emplacement spécifié
+    public Object getValueAt(int row, int col) {
+      return this.data[row][col];
+    }  
+    public String getColumnName(int col) {
+     return this.title[col];
+    }
+    //Retourne la classe de la donnée de la colonne
+     public Class getColumnClass(int column)
+            {
+                for (int row = 0; row < getRowCount(); row++)
+                {
+                    Object o = getValueAt(row, column);
+
+                    if (o != null)
+                    {
+                        return o.getClass();
+                    }
+                }
+
+                return Object.class;
+            }
+    //Retourne vrai si la cellule est éditable : celle-ci sera donc éditable
+    //Retourne vrai si la cellule est éditable : celle-ci sera donc éditable
+//Retourne vrai si la cellule est éditable : celle-ci sera donc éditable
+     //Retourne vrai si la cellule est éditable : celle-ci sera donc éditable
+	public boolean isCellEditable(int row, int col){
+	//On appelle la méthode getValueAt qui retourne la valeur d'une cellule
+	//Et on effectue un traitement spécifique si c'est un JButton
+	if(getValueAt(0, col) instanceof JButton)
+	return false;
+	return true; 
+	}
+	public void setValueAt(Object value, int row, int col) {
+	data[row][col] = value;
+	fireTableCellUpdated(row, col);
+	}
+	public void removeRow(int position){
+	   
+	  int indice = 0, indice2 = 0;
+	  int nbRow = this.getRowCount()-1;
+	  int nbCol = this.getColumnCount();
+	  Object temp[][] = new Object[nbRow][nbCol];
+	   
+	  for(Object[] value : this.data){
+	     if(indice != position){
+	        temp[indice2++] = value;
+	     }
+	     System.out.println("Indice = " + indice);
+	     indice++;
+	  }
+	  this.data = temp;
+	  temp = null;
+	  //Cette méthode permet d'avertir le tableau que les données
+	  //ont été modifiées, ce qui permet une mise à jour complète du tableau
+	  this.fireTableDataChanged();
+	}
+	
+	//Permet d'ajouter une ligne dans le tableau
+	public void addRow(Object[] data){
+	  int indice = 0, nbRow = this.getRowCount(), nbCol = this.getColumnCount();
+	   
+	  Object temp[][] = this.data;
+	  this.data = new Object[nbRow+1][nbCol];
+	   
+	  for(Object[] value : temp)
+	     this.data[indice++] = value;
+	   
+	      
+	  this.data[indice] = data;
+	  temp = null;
+	  //Cette méthode permet d'avertir le tableau que les données
+	  //ont été modifiées, ce qui permet une mise à jour complète du tableau
+	  this.fireTableDataChanged();
+	}
+
+
+}
+
+
+
